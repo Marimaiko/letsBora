f2bbc47846f946845b2cbcdc5cf218a5af6d9e9e
Davi Paiva <davialexandre_dap@hotmail.com>
2025-03-21 19:57:41 -0300

    ✨ feat(Views): Add HomeViewController
    - Added HomeViewController.swift to the Views group.
    - Configured SceneDelegate to temporarily use HomeViewController as the root view controller.

---
+++ LetsBora/LetsBora/Views/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  LetsBora
+//
+//  Created by Davi Paiva on 21/03/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
