a2174affae840479a383db60d41bad5ad18b9d22
Davi Paiva <davialexandre_dap@hotmail.com>
2025-03-21 20:14:19 -0300

    ✨ feat(HomeViewController): Add title label to HomeViewController
    - Added a title label "Let's Bora" to the HomeViewController.
    - Updated project settings to LastUpgradeCheck = 1620 and ENABLE_USER_SCRIPT_SANDBOXING = YES.

--- LetsBora/LetsBora/Views/HomeViewController.swift
+++ LetsBora/LetsBora/Views/HomeViewController.swift
@@ -8,9 +8,46 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    // MARK: - UI Components
+    lazy var titleLabel = createLabel(withText: "Let's Bora",fontSize: 48)
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
     }
-
+    
+    // MARK: - Setup View
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    private func setHierarchy(){
+        self.view.addSubview(titleLabel)
+    }
+    private func setConstraints(){
+        let constraints: [NSLayoutConstraint] = [
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 18),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    // MARK: - Factory Components
+    private func createLabel(withText text: String, fontSize: CGFloat = 17, weight: UIFont.Weight = .regular) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.font = .systemFont(ofSize: fontSize, weight: weight)
+        label.textColor = .black
+        return label
+    }
+    
 }
+
+// MARK: - Preview Profile
+@available(iOS 17.0,*)
+#Preview(traits: .portrait, body: {
+    HomeViewController()
+})
