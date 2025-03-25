05b54b7600fa2146bc189b2277647cd7aac47907
João Victor Almeida <joaovictorsilva.meida@gmail.com>
2025-03-24 23:23:33 -0300

    ✨ feat(ui): add CreateEventViewController
    -  Add preview.

---
+++ LetsBora/LetsBora/View/CreateEventViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CreateEventViewController.swift
+//  LetsBora
+//
+//  Created by João VIctir da Silva Almeida on 24/03/25.
+//
+
+import UIKit
+
+class CreateEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
+
+// MARK: - Preview Profile
+@available(iOS 17.0,*)
+#Preview(traits: .portrait, body: {
+    CreateEventViewController()
+})
