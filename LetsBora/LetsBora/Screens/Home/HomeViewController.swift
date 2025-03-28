72f633da83c131f952baa2153c7057f8f14210c5
Davi Paiva <davialexandre_dap@hotmail.com>
2025-03-28 17:33:18 -0300

    ♻️ refactor(Home): Refactor HomeViewController to load HomeView directly in loadView
    -  Replaced setupView, setHierarchy, and setConstraints with a loadView override to directly assign HomeView to the controller's view.

--- LetsBora/LetsBora/Screens/Home/HomeViewController.swift
+++ LetsBora/LetsBora/Screens/Home/HomeViewController.swift
@@ -9,37 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    // MARK: - UI Components
-    private lazy var homeView: HomeView = {
-        let view = HomeView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupView()
     }
     
-    // MARK: - Setup View
-    private func setupView() {
-        view.backgroundColor = .white  // Ensure background is not black
-        setHierarchy()
-        setConstraints()
-    }
-    
-    private func setHierarchy() {
-        view.addSubview(homeView)
-    }
-    
-    private func setConstraints() {
-        NSLayoutConstraint.activate([
-            homeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            homeView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            homeView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            homeView.bottomAnchor.constraint(equalTo: view.bottomAnchor)  // Ensures full screen usage
-        ])
+    override func  loadView() {
+        self.view = HomeView()
     }
 }
 
