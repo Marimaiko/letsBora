9c47600978501c6af5a87fa56e2f387d9c4706ee
João Victor Almeida <joaovictorsilva.meida@gmail.com>
2025-03-29 11:58:10 -0300

    ✨ feat(ui): add add first labels and configuration
    - Implement name and description.
    - Implement date handler.

--- LetsBora/LetsBora/View/CreateEventViewController.swift
+++ LetsBora/LetsBora/View/CreateEventViewController.swift
@@ -1,36 +1,120 @@
-//
-//  CreateEventViewController.swift
-//  LetsBora
-//
-//  Created by João VIctir da Silva Almeida on 24/03/25.
-//
-
 import UIKit
 
 class CreateEventViewController: UIViewController {
+    
+    //MARK: - UIComponents
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Criar Evento"
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .black
+        return label
+    } ()
+    
+    private lazy var nameEventTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Nome do evento"
+        textField.borderStyle = .roundedRect
+        textField.textColor = .black
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return textField
+    } ()
+    
+    private lazy var descriptionEventTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Descrição"
+        textField.borderStyle = .roundedRect
+        textField.textColor = .black
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        return textField
+    } ()
+    
+    private let dateTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Selecione uma data"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
 
+    private let datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .date
+        picker.preferredDatePickerStyle = .wheels
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        return picker
+    }()
+    
+    @objc private func doneButtonTapped() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        dateTextField.text = formatter.string(from: datePicker.date)
+        dateTextField.resignFirstResponder() // Esconde o teclado
+    }
+    
+    lazy var stackView: UIStackView = createStackView(
+        subViews: [
+            titleLabel,
+            nameEventTextField,
+            descriptionEventTextField,
+            dateTextField],
+        spacing: 16)
+
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1.0)
+        
+        // Configurando o DatePicker como inputView do TextField
+        dateTextField.inputView = datePicker
+            
+        // Criando uma Toolbar com botão "Concluído"
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+            
+        let doneButton = UIBarButtonItem(title: "Concluído", style: .done, target: self, action: #selector(doneButtonTapped))
+        toolbar.setItems([doneButton], animated: true)
+            
+        // Adicionando a toolbar ao teclado do TextField
+        dateTextField.inputAccessoryView = toolbar
+        setupLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Setup View
+    private func setupLayout() {
+        view.addSubview(stackView)
+        setConstraints()
+        
     }
-    */
+        
+    private func setConstraints(){
+        let constraints: [NSLayoutConstraint] = [
+            // Position stack view at the top of safe area with 16pt horizontal margins
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -16),
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
     
-
-}
-
-// MARK: - Preview Profile
-@available(iOS 17.0,*)
-#Preview(traits: .portrait, body: {
-    CreateEventViewController	()
-})
+    private func createStackView(subViews: [UIView], spacing: CGFloat = 8) -> UIStackView {
+            let stackView = UIStackView(arrangedSubviews: subViews)
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .vertical
+            stackView.spacing = spacing
+            return stackView
+        }
+        
+    }
+    
+    // MARK: - Preview Profile
+    @available(iOS 17.0,*)
+    #Preview(traits: .portrait, body: {
+        CreateEventViewController()
+    })
