ecad6b38c24ff4a2e98b626c043cdc53551f05ee
Mariana Maiko <marianam@MacBook-Pro-de-Mariana.local>
2025-04-30 18:35:13 -0300

    create registerView and registerViewController

---
+++ LetsBora/LetsBora/Screens/Register/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  LetsBora
+//
+//  Created by Mariana Maiko on 30/04/25.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        let view = RegisterView()
+        self.view = view
+    }
+}
