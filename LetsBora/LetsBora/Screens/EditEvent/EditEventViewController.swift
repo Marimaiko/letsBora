a356859f59f97d263af4b9e60d7eb482e6b83382
João Victor Almeida <joaovictorsilva.meida@gmail.com>
2025-05-26 22:43:46 -0300

    ✨ feat(ui): add folder and docs
    - Implement work folder and docs.

---
+++ LetsBora/LetsBora/Screens/EditEvent/EditEventViewController.swift
@@ -0,0 +1,12 @@
+//
+//  EditEventViewController.swift
+//  LetsBora
+//
+//  Created by João VIctir da Silva Almeida on 26/05/25.
+//
+
+import UIKit
+
+class EditEventViewController: UIViewController{
+    
+}
